c0708aa33c4401fa21955303ec1990e7e3f42c3b
phicho <phichoo@gmail.com>
2015-05-29 22:42:44 +0200

    init log4net for project

--- TestCrawlerAbot/Program.cs
+++ TestCrawlerAbot/Program.cs
@@ -1,9 +1,11 @@
-﻿namespace TestCrawlerAbot
+﻿using log4net.Config;
+namespace TestCrawlerAbot
 {
     class Program
     {
         private static void Main(string[] args)
         {
+            XmlConfigurator.Configure();
             var crawler = new CrawlerWrapper();
             crawler.Crawl("http://www.savana.com.mk/");
         }
