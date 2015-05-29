bde0af6743952f54a0ccaec6074e923b67bd573c
phicho <phichoo@gmail.com>
2015-05-29 21:53:30 +0200

    intial start up project

---
+++ TestCrawlerAbot/CrawlerWrapper.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abot.Crawler;
+using Abot.Poco;
+using System.Net;
+
+
+namespace TestCrawlerAbot
+{
+    class CrawlerWrapper
+    {
+        #region fields
+        private readonly PoliteWebCrawler _crawler = new PoliteWebCrawler();
+        #endregion
+        #region Ctors
+        public CrawlerWrapper()
+        {
+            _crawler.PageCrawlStartingAsync += crawler_ProcessPageCrawlStarting;
+            _crawler.PageCrawlCompletedAsync += crawler_ProcessPageCrawlCompleted;
+            _crawler.PageCrawlDisallowedAsync += crawler_PageCrawlDisallowed;
+            _crawler.PageLinksCrawlDisallowedAsync += crawler_PageLinksCrawlDisallowed;
+        }
+        #endregion
+
+        #region Public Methods
+
+        public void Crawl(string url)
+        {
+            var result = _crawler.Crawl(new Uri(url));
+
+            if (result.ErrorOccurred)
+            {
+                Console.WriteLine("Crawl of {0} completed with error: {1}", result.RootUri.AbsoluteUri, result.ErrorException.Message);
+            }
+            else
+            {
+                Console.WriteLine("Crawl of {0} completed without error.", result.RootUri.AbsoluteUri);
+            }
+        }
+        #endregion
+
+        #region Private Methods
+        private static void crawler_ProcessPageCrawlStarting(object sender, PageCrawlStartingArgs e)
+        {
+            var pageToCrawl = e.PageToCrawl;
+            Console.WriteLine("About to crawl link {0} which was found on page {1}", pageToCrawl.Uri.AbsoluteUri, pageToCrawl.ParentUri.AbsoluteUri);
+        }
+
+        private static void crawler_ProcessPageCrawlCompleted(object sender, PageCrawlCompletedArgs e)
+        {
+            CrawledPage crawledPage = e.CrawledPage;
+
+            if (crawledPage.WebException != null || crawledPage.HttpWebResponse.StatusCode != HttpStatusCode.OK)
+                Console.WriteLine("Crawl of page failed {0}", crawledPage.Uri.AbsoluteUri);
+            else
+                Console.WriteLine("Crawl of page succeeded {0}", crawledPage.Uri.AbsoluteUri);
+
+            if (string.IsNullOrEmpty(crawledPage.Content.Text))
+                Console.WriteLine("Page had no content {0}", crawledPage.Uri.AbsoluteUri);
+        }
+
+        private static void crawler_PageLinksCrawlDisallowed(object sender, PageLinksCrawlDisallowedArgs e)
+        {
+            CrawledPage crawledPage = e.CrawledPage;
+            Console.WriteLine("Did not crawl the links on page {0} due to {1}", crawledPage.Uri.AbsoluteUri, e.DisallowedReason);
+        }
+
+        private static void crawler_PageCrawlDisallowed(object sender, PageCrawlDisallowedArgs e)
+        {
+            PageToCrawl pageToCrawl = e.PageToCrawl;
+            Console.WriteLine("Did not crawl page {0} due to {1}", pageToCrawl.Uri.AbsoluteUri, e.DisallowedReason);
+        }
+        #endregion
+    }
+}
---
+++ TestCrawlerAbot/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace TestCrawlerAbot
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            var crawler = new CrawlerWrapper();
+            crawler.Crawl("http://www.savana.com.mk/");
+        }
+    }
+}
